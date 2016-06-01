04cbf298af0556e254dd76f786db2bc7f2db215f
JeremieDrazic <jerem.drazic@gmail.com>
2016-06-01 12:22:59 +0200

    planktons

--- src/sections/atlantis/experience/world/objects/planktons/planktons.js
+++ src/sections/atlantis/experience/world/objects/planktons/planktons.js
@@ -19,10 +19,10 @@ export default class Planktons extends THREE.Object3D {
             this.systems.push(planktonSystem)
         })
 
-        this.systems[0].position.set(1, -9, 1)
-        this.systems[1].position.set(1, -8, 2)
-        this.systems[2].position.set(-1, -8, 2)
-        this.systems[3].position.set(-1, -9, 1)
+        this.systems[0].position.set(1.5, -9, 1)
+        this.systems[1].position.set(1.5, -8, 2)
+        this.systems[2].position.set(-1.5, -8, 2)
+        this.systems[3].position.set(-1.5, -9, 1)
 
         this.systems[4].position.set(0, -9, 0)
     }
@@ -33,31 +33,31 @@ export default class Planktons extends THREE.Object3D {
 
         tlRight.to(this.systems[0].material.uniforms.size, 1.5, {
             value: 40.0
-        }, '+=2').fromTo(this.systems[0].material.uniforms.size, 1.5, {
+        }).fromTo(this.systems[0].material.uniforms.size, 1.5, {
             value: 40.0
         }, {
             value: 5.0
         }).to(this.systems[1].material.uniforms.size, 1.5, {
             value: 40.0
-        }, '-=3.5').fromTo(this.systems[1].material.uniforms.size, 1.5, {
+        }).fromTo(this.systems[1].material.uniforms.size, 1.5, {
             value: 40.0
         }, {
             value: 5.0
-        }, '-=5')
+        },)
 
-        tlLeft.to(this.systems[2].material.uniforms.size, 1.5, {
+        tlRight.to(this.systems[2].material.uniforms.size, 1.5, {
             value: 40.0
-        }, '+=4').fromTo(this.systems[2].material.uniforms.size, 1.5, {
+        }).fromTo(this.systems[2].material.uniforms.size, 1.5, {
             value: 40.0
         }, {
             value: 5.0
         }).to(this.systems[3].material.uniforms.size, 1.5, {
             value: 40.0
-        }, '-=6').fromTo(this.systems[3].material.uniforms.size, 1.5, {
+        }).fromTo(this.systems[3].material.uniforms.size, 1.5, {
             value: 40.0
         }, {
             value: 5.0
-        }, '-=8')
+        })
     }
 
     update(frame) {
