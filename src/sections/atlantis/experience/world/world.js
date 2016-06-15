f03b8daad1449c8d7802b66f9e41758c4fe4c78d
robin-dela <delaporte.ro@gmail.com>
2016-06-15 09:37:29 +0200

    remove toon pass

--- src/sections/atlantis/experience/world/world.js
+++ src/sections/atlantis/experience/world/world.js
@@ -42,7 +42,6 @@ import gui                from 'helpers/app/gui'
 import WAGNER             from '@alex_toudic/wagner'
 import FXAAPass           from '@alex_toudic/wagner/src/passes/fxaa/FXAAPass'
 import MultiPassBloomPass from '@alex_toudic/wagner/src/passes/bloom/MultiPassBloomPass'
-import ToonPass           from '@alex_toudic/wagner/src/passes/toon/ToonPass'
 import NoisePass          from '@alex_toudic/wagner/src/passes/noise/noise'
 
 export class World {
@@ -217,11 +216,6 @@ export class World {
         })
         this.multiPassBloomPass.enabled = true
         this.passes.push(this.multiPassBloomPass)
-
-        //TOONPASS
-        this.toonPass = new ToonPass()
-        this.toonPass.enabled = false
-        this.passes.push(this.toonPass)
 
         //NOISEPASS
         this.noisePass = new NoisePass({
